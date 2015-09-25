44305a596ff7116b742f512625cf4503356486a2
Denis Kurilenko <kurilenkodenis@gmail.com>
2015-09-25 11:36:29 +0300

    added new MonoAndroid to NuGet package, added new examples for SDKs

---
+++ Examples/Virgil.Basics/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Virgil.Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var quickstartTask = new Quickstart();
+            quickstartTask.Run().Wait();
+        }
+    }
+}
