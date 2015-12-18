154189b53807ee245c745269ac1d09e3c05c4b92
Denis Kurilenko <kurilenkodenis@gmail.com>
2015-12-18 16:02:19 +0200

    Develop C# .NET Enhancement Class Wrapper for Crypto Library

---
+++ Crypto/Source/Virgil.Crypto.Wrapper.Tests/VirgilCryptoTests.cs
@@ -0,0 +1,221 @@
+﻿namespace Virgil.Crypto.Wrapper.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class VirgilCryptoTests
+    {
+        [Test]
+        public void Should_GenerateKeyPairWithEncryptedPrivateKey()
+        {
+            var keyPair = CryptoHelper.GenerateKeyPair("Password");
+            Encoding.UTF8.GetString(keyPair.PrivateKey()).Should().Contain("ENCRYPTED");
+        }
+
+        [Test]
+        public void Should_GenerateKeyPairWithPurePrivateKey()
+        {
+            var keyPair = CryptoHelper.GenerateKeyPair();
+            Encoding.UTF8.GetString(keyPair.PrivateKey()).Should().NotContain("ENCRYPTED");
+        }
+
+        [Test]
+        public void Should_ReturnEncryptedStringInBase64Format_When_EncryptingWithKey()
+        {
+            const string text = "Encrypt me!!!";
+
+            var keyPair = CryptoHelper.GenerateKeyPair();
+            var cipherTextBase64 = CryptoHelper.Encrypt(text, "Recipient", keyPair.PublicKey());
+
+            Utils.IsBase64Encoded(cipherTextBase64).Should().BeTrue();
+        }
+
+        [Test]
+        public void Should_ReturnEncryptedStringInBase64Format_When_EncryptingWithPassword()
+        {
+            const string text = "Encrypt me!!!";
+            var cipherTextBase64 = CryptoHelper.Encrypt(text, "Password");
+
+            Utils.IsBase64Encoded(cipherTextBase64).Should().BeTrue();
+        }
+        
+        [Test]
+        public void Should_EncryptTextForMultipleRecipients()
+        {
+            const string text = "Encrypt me!!!";
+
+            var keyPair1 = new VirgilKeyPair();
+            var keyPair2 = new VirgilKeyPair();
+            var keyPair3 = new VirgilKeyPair();
+
+            var cipherTextBase64 = CryptoHelper.Encrypt(text, new Dictionary<string, byte[]>
+            {
+                { "Recipient1", keyPair1.PublicKey() },
+                { "Recipient2", keyPair2.PublicKey() },
+                { "Recipient3", keyPair3.PublicKey() }
+            });
+
+            using (var cipher = new VirgilCipher())
+            {
+                var cipherTextData = Convert.FromBase64String(cipherTextBase64);
+
+                Encoding.UTF8.GetString(cipher.DecryptWithKey(cipherTextData, 
+                    Encoding.UTF8.GetBytes("Recipient1"), keyPair1.PrivateKey())).Should().Be(text);
+
+                Encoding.UTF8.GetString(cipher.DecryptWithKey(cipherTextData,
+                    Encoding.UTF8.GetBytes("Recipient2"), keyPair2.PrivateKey())).Should().Be(text);
+
+                Encoding.UTF8.GetString(cipher.DecryptWithKey(cipherTextData,
+                    Encoding.UTF8.GetBytes("Recipient3"), keyPair3.PrivateKey())).Should().Be(text);
+            }
+        }
+
+        [Test]
+        public void Should_EncryptTextForOneRecipient()
+        {
+            const string text = "Encrypt me!!!";
+
+            var keyPair1 = new VirgilKeyPair();
+
+            var cipherTextBase64 = CryptoHelper.Encrypt(text, "Recipient1", keyPair1.PublicKey());
+
+            using (var cipher = new VirgilCipher())
+            {
+                var cipherTextData = Convert.FromBase64String(cipherTextBase64);
+
+                Encoding.UTF8.GetString(cipher.DecryptWithKey(cipherTextData,
+                    Encoding.UTF8.GetBytes("Recipient1"), keyPair1.PrivateKey())).Should().Be(text);
+            }
+        }
+
+        [Test]
+        public void Should_EncryptTextWithPassword()
+        {
+            const string text = "Encrypt me!!!";
+
+            var cipherTextBase64 = CryptoHelper.Encrypt(text, "Password");
+            using (var cipher = new VirgilCipher())
+            {
+                var cipherTextData = Convert.FromBase64String(cipherTextBase64);
+                Encoding.UTF8.GetString(cipher.DecryptWithPassword(cipherTextData, Encoding.UTF8.GetBytes("Password"))).Should().Be(text);
+            }
+        }
+
+        [Test, ExpectedException(typeof(FormatException))]
+        public void Should_ThrowException_When_DecryptedCipherStringIsNotInBase64Format()
+        {
+            const string text = "NOT Base64 Encoded String";
+            var keyPair = new VirgilKeyPair();
+
+            CryptoHelper.Decrypt(text, "Recipient1", keyPair.PrivateKey());
+        }
+
+        [Test]
+        public void Should_DecryptCipherTextWithPassword()
+        {
+            using (var cipher = new VirgilCipher())
+            {
+                const string text = "Encrypt me!!!";
+
+                var passwordData = Encoding.UTF8.GetBytes("Password");
+                var textData = Encoding.UTF8.GetBytes(text);
+                cipher.AddPasswordRecipient(passwordData);
+
+                var cipherData = cipher.Encrypt(textData, true);
+
+                Encoding.UTF8.GetString(CryptoHelper.Decrypt(cipherData, "Password")).Should().Be(text);
+            }
+        }
+
+        [Test]
+        public void Should_DecryptCipherTextWithPrivateKey()
+        {
+            using (var cipher = new VirgilCipher())
+            {
+                const string text = "Encrypt me!!!";
+
+                var keyPair = new VirgilKeyPair();
+                var recipientIdData = Encoding.UTF8.GetBytes("Default");
+                var textData = Encoding.UTF8.GetBytes(text);
+                cipher.AddKeyRecipient(recipientIdData, keyPair.PublicKey());
+
+                var cipherData = cipher.Encrypt(textData, true);
+
+                Encoding.UTF8.GetString(CryptoHelper.Decrypt(cipherData, "Default", keyPair.PrivateKey())).Should().Be(text);
+            }
+        }
+
+        [Test]
+        public void Should_DecryptCipherTextWithEncryptedPrivateKey()
+        {
+            using (var cipher = new VirgilCipher())
+            {
+                const string text = "Encrypt me!!!";
+                const string password = "Password";
+
+                var keyPair = new VirgilKeyPair(Encoding.UTF8.GetBytes(password));
+                var recipientIdData = Encoding.UTF8.GetBytes("Default");
+                var textData = Encoding.UTF8.GetBytes(text);
+                cipher.AddKeyRecipient(recipientIdData, keyPair.PublicKey());
+
+                var cipherData = cipher.Encrypt(textData, true);
+
+                Encoding.UTF8.GetString(CryptoHelper.Decrypt(cipherData, "Default", keyPair.PrivateKey(), password)).Should().Be(text);
+            }
+        }
+
+        [Test]
+        public void Should_ReturnSignInBase64Format()
+        {
+            const string textToSign = "Sign me!!!";
+
+            var keyPair = new VirgilKeyPair();
+            Utils.IsBase64Encoded(CryptoHelper.Sign(textToSign, keyPair.PrivateKey())).Should().BeTrue();
+        }
+
+        [Test]
+        public void Should_ThrowException_WhenReturnSignInBase64Format()
+        {
+            const string textToSign = "Sign me!!!";
+
+            var keyPair = new VirgilKeyPair();
+            Utils.IsBase64Encoded(CryptoHelper.Sign(textToSign, keyPair.PrivateKey())).Should().BeTrue();
+        }
+
+        [Test]
+        public void Should_CreateDigitalSignatureForTextWithEncryptedPassword()
+        {
+            const string textToSign = "Sign me!!!";
+            var data = Encoding.UTF8.GetBytes(textToSign);
+            var keyPair = new VirgilKeyPair(Encoding.UTF8.GetBytes("Password"));
+
+            using (var signer = new Virgil.Crypto.VirgilSigner())
+            {
+                var signData = signer.Sign(data, keyPair.PrivateKey(), Encoding.UTF8.GetBytes("Password"));
+                var sign = Convert.ToBase64String(signData);
+
+                CryptoHelper.Sign(textToSign, keyPair.PrivateKey(), "Password").Should().Be(sign);
+            }
+        }
+
+        [Test]
+        public void Should_VerifyDigitalSignatureCreatedWithNativeLibrary()
+        {
+            const string textToSign = "Sign me!!!";
+            var data = Encoding.UTF8.GetBytes(textToSign);
+            var keyPair = new VirgilKeyPair();
+
+            using (var signer = new Virgil.Crypto.VirgilSigner())
+            {
+                var signData = signer.Sign(data, keyPair.PrivateKey());
+                var signBase64 = Convert.ToBase64String(signData);
+
+                CryptoHelper.Verify(textToSign, signBase64, keyPair.PublicKey()).Should().BeTrue();
+            }
+        }
+    }
+}
---
+++ Crypto/Source/Virgil.Crypto.Wrapper/CryptoHelper.cs
@@ -0,0 +1,246 @@
+﻿namespace Virgil.Crypto.Wrapper
+{
+    public class CryptoHelper
+    {
+        /// <summary>
+        /// Encrypts text for recipient's public key.
+        /// </summary>
+        /// <param name="text">The text to be encrypted</param>
+        /// <param name="recipientId">The recipient's unique identifier.</param>
+        /// <param name="recipientPublicKey">The recipient's public key data.</param>
+        /// <returns>Encrypted text in Base64 format</returns>
+        public static string Encrypt(string text, string recipientId, byte[] recipientPublicKey)
+        {
+            var textData = System.Text.Encoding.UTF8.GetBytes(text);
+
+            var recipients = new System.Collections.Generic.Dictionary<string, byte[]> { { recipientId, recipientPublicKey } };
+            var cipherDataBase64 = System.Convert.ToBase64String(Encrypt(textData, recipients));
+            return cipherDataBase64;
+        }
+
+        /// <summary>
+        /// Encrypts text for multiple recipient.
+        /// </summary>
+        /// <param name="text">The text to be encrypted</param>
+        /// <param name="recipients">Dictionary of recipients public keys</param>
+        /// <returns>Encrypted text in Base64 format</returns>
+        public static string Encrypt(string text, System.Collections.Generic.IDictionary<string, byte[]> recipients)
+        {
+            var textData = System.Text.Encoding.UTF8.GetBytes(text);
+            var cipherDataBase64 = System.Convert.ToBase64String(Encrypt(textData, recipients));
+            return cipherDataBase64;
+        }
+
+        /// <summary>
+        /// Encrypts text with password.
+        /// </summary>
+        /// <param name="text">The text to be encrypted</param>
+        /// <param name="password">The password</param>
+        /// <returns>Encrypted text in Base64 format</returns>
+        public static string Encrypt(string text, string password)
+        {
+            var textData = System.Text.Encoding.UTF8.GetBytes(text);
+            var cipherDataBase64 = System.Convert.ToBase64String(Encrypt(textData, password));
+            return cipherDataBase64;
+        }
+
+        /// <summary>
+        /// Encrypts text for multiple recipient.
+        /// </summary>
+        /// <param name="data">The data to be encrypted</param>
+        /// <param name="recipients">Dictionary of recipients public keys</param>
+        /// <returns>Encrypted data</returns>
+        public static byte[] Encrypt(byte[] data, System.Collections.Generic.IDictionary<string, byte[]> recipients)
+        {
+            using (var cipher = new VirgilCipher())
+            {
+                foreach (var recipient in recipients)
+                {
+                    var recipientIdData = System.Text.Encoding.UTF8.GetBytes(recipient.Key);
+                    cipher.AddKeyRecipient(recipientIdData, recipient.Value);
+                }
+
+                var cipherData = cipher.Encrypt(data, true);
+                return cipherData;
+            }
+        }
+
+        /// <summary>
+        /// Encrypts text with password.
+        /// </summary>
+        /// <param name="data">The data to be encrypted</param>
+        /// <param name="password">The password</param>
+        /// <returns>Encrypted data</returns>
+        public static byte[] Encrypt(byte[] data, string password)
+        {
+            using (var cipher = new VirgilCipher())
+            {
+                var passwordData = System.Text.Encoding.UTF8.GetBytes(password);
+                cipher.AddPasswordRecipient(passwordData);
+
+                var cipherData = cipher.Encrypt(data, true);
+
+                return cipherData;
+            }
+        }
+
+        /// <summary>
+        /// Decrypts encrypted text for recipient with private key.
+        /// </summary>
+        /// <param name="cipherTextBase64">The encrypted text</param>
+        /// <param name="recipientId">The recipient's unique identifier</param>
+        /// <param name="privateKey">The private key</param>
+        /// <param name="privateKeyPassword">The Private Key's password</param>
+        /// <returns>Decrypted text</returns>
+        public static string Decrypt(string cipherTextBase64, string recipientId, byte[] privateKey, string privateKeyPassword = null)
+        {
+            var cipherData = System.Convert.FromBase64String(cipherTextBase64);
+            var textData = Decrypt(cipherData, recipientId, privateKey);
+            var text = System.Text.Encoding.UTF8.GetString(textData, 0, textData.Length);
+            return text;
+        }
+        
+        /// <summary>
+        /// Decrypts encrypted text with password. 
+        /// </summary>
+        /// <param name="cipherTextBase64">Encrypted text in Base64 format</param>
+        /// <param name="password">Encrypted text password</param>
+        /// <returns>Decrypted text</returns>
+        public static string Decrypt(string cipherTextBase64, string password)
+        {
+            var cipherData = System.Convert.FromBase64String(cipherTextBase64);
+
+            var textData = Decrypt(cipherData, password);
+            var text = System.Text.Encoding.UTF8.GetString(textData, 0, textData.Length);
+
+            return text;
+        }
+
+        /// <summary>
+        /// Decrypts cipher data for recipient ID with private key.
+        /// </summary>
+        /// <param name="cipherData">Encrypted data</param>
+        /// <param name="recipientId">The recipient's unique identifier</param>
+        /// <param name="privateKey">The Private Key</param>
+        /// <param name="privateKeyPassword">The Private Key's password</param>
+        /// <returns>Decrypted data</returns>
+        public static byte[] Decrypt(byte[] cipherData, string recipientId, byte[] privateKey, string privateKeyPassword = null)
+        {
+            using (var cipher = new Virgil.Crypto.VirgilCipher())
+            {
+                var recipientIdData = System.Text.Encoding.UTF8.GetBytes(recipientId);
+
+                byte[] textData;
+
+                if (privateKeyPassword == null)
+                {
+                    textData = cipher.DecryptWithKey(cipherData, recipientIdData, privateKey);
+                }
+                else
+                {
+                    var privateKeyPasswordData = System.Text.Encoding.UTF8.GetBytes(privateKeyPassword);
+                    textData = cipher.DecryptWithKey(cipherData, recipientIdData, privateKey, privateKeyPasswordData);
+                }
+
+                return textData;
+            }
+        }
+
+        /// <summary>
+        /// Decrypts encrypted data with password. 
+        /// </summary>
+        /// <param name="cipherData">Encrypted data</param>
+        /// <param name="password">Encrypted text password</param>
+        /// <returns>Decrypted data</returns>
+        public static byte[] Decrypt(byte[] cipherData, string password)
+        {
+            using (var cipher = new Virgil.Crypto.VirgilCipher())
+            {
+                var passwordData = System.Text.Encoding.UTF8.GetBytes(password);
+                var textData = cipher.DecryptWithPassword(cipherData, passwordData);
+                return textData;
+            }
+        }
+
+        /// <summary>
+        /// Creates digital signature for passing data using private key.
+        /// </summary>
+        /// <param name="text">Data to be signed.</param>
+        /// <param name="privateKey">The Private Key</param>
+        /// <param name="privateKeyPassword">The private key password</param>
+        /// <returns>The digital signature data</returns>
+        public static string Sign(string text, byte[] privateKey, string privateKeyPassword = null)
+        {
+            var textData = System.Text.Encoding.UTF8.GetBytes(text);
+
+            var signData = Sign(textData, privateKey, privateKeyPassword);
+            var signBase64 = System.Convert.ToBase64String(signData);
+            return signBase64;
+        }
+
+        /// <summary>
+        /// Creates digital signature for passing data using private key.
+        /// </summary>
+        /// <param name="data">Data to be signed.</param>
+        /// <param name="privateKey">The Private Key</param>
+        /// <param name="privateKeyPassword">The private key password</param>
+        /// <returns>The digital signature data</returns>
+        public static byte[] Sign(byte[] data, byte[] privateKey, string privateKeyPassword = null)
+        {
+            using (var signer = new Virgil.Crypto.VirgilSigner())
+            {
+                var signData = privateKeyPassword == null
+                    ? signer.Sign(data, privateKey)
+                    : signer.Sign(data, privateKey, System.Text.Encoding.UTF8.GetBytes(privateKeyPassword));
+
+                return signData;
+            }
+        }
+
+        /// <summary>
+        /// Virifies the original text digital signature for owner's public key.
+        /// </summary>
+        /// <param name="text">The original text</param>
+        /// <param name="signBase64">Digital signature in Base64 format</param>
+        /// <param name="publicKey">The public key</param>
+        /// <returns>True, if digital signtature is valid, otherwise False.</returns>
+        public static bool Verify(string text, string signBase64, byte[] publicKey)
+        {
+            using (var signer = new Virgil.Crypto.VirgilSigner())
+            {
+                var textData = System.Text.Encoding.UTF8.GetBytes(text);
+                var signData = System.Convert.FromBase64String(signBase64);
+
+                var isValid = Verify(textData, signData, publicKey);
+                return isValid;
+            }
+        }
+
+        /// <summary>
+        /// Virifies the original data digital signature for owner's public key.
+        /// </summary>
+        /// <param name="data">The original data</param>
+        /// <param name="signData">Digital signature data</param>
+        /// <param name="publicKey">The public key</param>
+        /// <returns>True, if digitar signtature is valid, otherwise False.</returns>
+        public static bool Verify(byte[] data, byte[] signData, byte[] publicKey)
+        {
+            using (var signer = new Virgil.Crypto.VirgilSigner())
+            {
+                var isValid = signer.Verify(data, signData, publicKey);
+                return isValid;
+            }
+        }
+
+        public static Virgil.Crypto.VirgilKeyPair GenerateKeyPair(string password = null)
+        {
+            if (password == null)
+            {
+                return new VirgilKeyPair();
+            }
+            
+            var passwordData = System.Text.Encoding.UTF8.GetBytes(password);
+            return new VirgilKeyPair(passwordData);
+        }
+    }
+}
