e3f3b3f4d060aff4e808ff71b53f167b2ebbefc1
Dmitry Kushnir <dmykush@gmail.com>
2016-01-04 15:11:22 +0200

    missing files

---
+++ SDK/Source/Virgil.SDK.Keys/Clients/VirgilApplicationIds.cs
@@ -0,0 +1,23 @@
+namespace Virgil.SDK.Keys.Clients
+{
+    /// <summary>
+    /// Holds known Virgil application ids
+    /// </summary>
+    public class VirgilApplicationIds
+    {
+        /// <summary>
+        /// Identity service app id
+        /// </summary>
+        public const string IdentityService = "com.virgilsecurity.pass";
+
+        /// <summary>
+        /// Private key service app id
+        /// </summary>
+        public const string PrivateService = "com.virgilsecurity.private-keys";
+
+        /// <summary>
+        /// Public service app id
+        /// </summary>
+        public const string PublicService = "com.virgilsecurity.keys";
+    }
+}
