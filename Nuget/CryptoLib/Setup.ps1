7d3db0f5c80bd5732efaf6bbdd815234962502d8
Denis Kurilenko <kurilenkodenis@gmail.com>
2016-01-15 14:54:26 +0200

    jenkins job script fixes

--- Nuget/CryptoLib/Setup.ps1
+++ Nuget/CryptoLib/Setup.ps1
@@ -25,9 +25,8 @@ function SmartCopy {
 # Create temporary directories
 
 $CurrentDir = Get-Location
-$TempDir    = New-Item -ItemType Directory -Force -Path "$CurrentDir\temp"
-$ExtractDir = New-Item -ItemType Directory -Force -Path "$CurrentDir\temp\extract"
-$PackageDir = New-Item -ItemType Directory -Force -Path "$CurrentDir\temp\package"
+$ExtractDir = New-Item -ItemType Directory -Force -Path "$CurrentDir\extract"
+$PackageDir = New-Item -ItemType Directory -Force -Path "$CurrentDir\package"
 
 # Extract current version form the file
 
@@ -40,28 +39,26 @@ $PortablePackageName    = "virgil-crypto-$CryptoLibVersion-net-windows-6.2"
 $MonoAndroidPackageName = "virgil-crypto-$CryptoLibVersion-mono-android-21"
 $MonoTouchPackageName   = "virgil-crypto-$CryptoLibVersion-mono-ios-7.0"
 
-..\Tools\TarTool\TarTool.exe "$TempDir\install\net\$MonoAndroidPackageName.tgz" $ExtractDir
-..\Tools\TarTool\TarTool.exe "$TempDir\install\net\$MonoTouchPackageName.tgz" $ExtractDir
+.\Tools\TarTool.exe ".\net\$MonoAndroidPackageName.tgz" $ExtractDir
+.\Tools\TarTool.exe ".\net\$MonoTouchPackageName.tgz" $ExtractDir
 
-Expand-ZIPFile -File "$TempDir\install\net\$PortablePackageName.zip" -Destination $ExtractDir
+Expand-ZIPFile -File ".\net\$PortablePackageName.zip" -Destination $ExtractDir
 
 # Prepare package
 # -------------------------------------------------------------------------------------------------------------
 
 SmartCopy "$ExtractDir\$MonoAndroidPackageName\lib\Virgil.Crypto.dll" "$PackageDir\lib\MonoAndroid\Virgil.Crypto.dll"
 SmartCopy "$ExtractDir\$MonoAndroidPackageName\lib\virgil_crypto_java_jni.jar" "$PackageDir\build\native\android\virgil_crypto_java_jni.jar"
-SmartCopy "$CurrentDir\targets\MonoAndroid.targets" "$PackageDir\build\MonoAndroid\Virgil.Crypto.targets"
+SmartCopy "$CurrentDir\MonoAndroid.targets" "$PackageDir\build\MonoAndroid\Virgil.Crypto.targets"
 
 SmartCopy "$ExtractDir\$MonoTouchPackageName\lib\Virgil.Crypto.dll" "$PackageDir\lib\MonoTouch\Virgil.Crypto.dll"
 SmartCopy "$ExtractDir\$MonoTouchPackageName\lib\libvirgil_crypto_net.so" "$PackageDir\build\native\ios\libvirgil_crypto_net.so"
-SmartCopy "$CurrentDir\targets\MonoTouch.targets" "$PackageDir\build\MonoTouch\Virgil.Crypto.targets"
+SmartCopy "$CurrentDir\MonoTouch.targets" "$PackageDir\build\MonoTouch\Virgil.Crypto.targets"
 
 SmartCopy "$ExtractDir\$PortablePackageName\lib\Virgil.Crypto.dll" "$PackageDir\lib\portable-net4+sl4+wp7+win8+wpa81\Virgil.Crypto.dll"
 SmartCopy "$ExtractDir\$PortablePackageName\lib\x64\virgil_crypto_net.dll" "$PackageDir\build\native\win\x64\virgil_crypto_net.dll"
 SmartCopy "$ExtractDir\$PortablePackageName\lib\x86\virgil_crypto_net.dll" "$PackageDir\build\native\win\x86\virgil_crypto_net.dll"
-SmartCopy "$CurrentDir\targets\PortableNet.targets" "$PackageDir\build\portable-net4+sl4+wp7+win8+wpa81\Virgil.Crypto.targets"
-
-SmartCopy "$CurrentDir\Package.nuspec" "$PackageDir\Package.nuspec"
+SmartCopy "$CurrentDir\PortableNet.targets" "$PackageDir\build\portable-net4+sl4+wp7+win8+wpa81\Virgil.Crypto.targets"
 
 # Replace version 
 (Get-Content "$CurrentDir\Package.nuspec").replace("%version%", $CryptoLibVersion) | Set-Content "$PackageDir\Package.nuspec"
