5ae8bf845cdfb4277b6f849eb6ae8f5d5b9b92d2
Denis Kurilenko <kurilenkodenis@gmail.com>
2016-04-28 12:15:26 +0200

    Changed version to 3.2.1

--- SDK/Source/Virgil.SDK/Properties/AssemblyInfo.cs
+++ SDK/Source/Virgil.SDK/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.2.0.0")]
-[assembly: AssemblyFileVersion("3.2.0.0")]
+[assembly: AssemblyVersion("3.2.1.0")]
+[assembly: AssemblyFileVersion("3.2.1.0")]
 
 [assembly: InternalsVisibleTo("Virgil.SDK.Keys.Tests")]
 [assembly: InternalsVisibleTo("Virgil.SDK.Domain")]
