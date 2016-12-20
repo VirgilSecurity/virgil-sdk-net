7e0ce5b3db67f94895dfda2ea7e3801eeb849344
Denis Kurilenko <Denis Kurilenko>
2016-12-20 04:06:37 +0200

    [SDK-123] Add default reference Virgil.Crypto

--- Nuget/virgil-sdk-crypto.py
+++
@@ -1,8 +0,0 @@
-import msbuilder
-
-proj = r'..\SDK\Source\Virgil.SDK.Crypto\Virgil.SDK.Crypto.csproj'
-output = r'.\output'
-
-builder = msbuilder.MsBuilder()
-builder.build(proj)
-builder.pack(proj, output)
