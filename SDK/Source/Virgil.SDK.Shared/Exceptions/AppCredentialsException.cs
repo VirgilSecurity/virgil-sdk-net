c4a7f67fe01bd19566b08a886d8f5055eae2d685
Vasilina Bezuglaya <Vasilina Bezuglaya>
2017-04-03 06:52:38 -0700

    [SDK-180] throw an exception in publish and revoke actions if app credentials are empty

---
+++ SDK/Source/Virgil.SDK.Shared/Exceptions/AppCredentialsException.cs
@@ -0,0 +1,18 @@
+﻿namespace Virgil.SDK.Exceptions
+{
+    /// <summary>
+    /// Represents errors occurred during application authentication.
+    /// </summary>
+    /// <seealso cref="Virgil.SDK.Exceptions.VirgilException" />
+    public class AppCredentialsException : VirgilException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppCredentialsException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public AppCredentialsException() : 
+            base("This action requires AppID and AppKey retrieved from development deshboard.")
+        {
+        }
+    }
+}
---
+++ SDK/Source/Virgil.SDK.Tests/CardsManagerTests.cs
@@ -0,0 +1,51 @@
+﻿namespace Virgil.SDK.Tests
+{
+    using System.Configuration;
+    using NUnit.Framework;
+    using FluentAssertions;
+    using System.Threading.Tasks;
+
+    class CardsManagerTests
+    {
+        [Test]
+        public async Task PublishCardWithAppCredentials_PredefinedCard_ShouldPublishCard()
+        {
+            var virgil = new VirgilApi(new VirgilApiContext
+            {
+                AccessToken = ConfigurationManager.AppSettings["virgil:AppAccessToken"],
+                Credentials = new AppCredentials
+                {
+                    AppId = ConfigurationManager.AppSettings["virgil:AppID"],
+                    AppKey = VirgilBuffer.FromFile(ConfigurationManager.AppSettings["virgil:AppKeyPath"]),
+                    AppKeyPassword = ConfigurationManager.AppSettings["virgil:AppKeyPassword"]
+                }
+            });
+            var aliceKey = virgil.Keys.Generate();
+            var aliceCard = virgil.Cards.Create("AliceCard", aliceKey);
+
+            await virgil.Cards.PublishAsync(aliceCard);
+            aliceCard.Id.Should().NotBeEmpty();
+            await virgil.Cards.RevokeAsync(aliceCard);
+        }
+
+        [Test]
+        public void PublishCardWithEmptyAppCredentials_PredefinedCard_ShouldOccurAppCredentialsException()
+        {
+            var AppAccessToken = ConfigurationManager.AppSettings["virgil:AppAccessToken"];
+            var virgil = new VirgilApi(AppAccessToken);
+            var aliceKey = virgil.Keys.Generate();
+            var aliceCard = virgil.Cards.Create("AliceCard", aliceKey);
+            Assert.ThrowsAsync<Exceptions.AppCredentialsException>(() => virgil.Cards.PublishAsync(aliceCard));
+        }
+
+        [Test]
+        public void RevokeWithEmptyAppCredentials_PredefinedCard_ShouldOccurAppCredentialsException()
+        {
+            var AppAccessToken = ConfigurationManager.AppSettings["virgil:AppAccessToken"];
+            var virgil = new VirgilApi(AppAccessToken);
+            var aliceKey = virgil.Keys.Generate();
+            var aliceCard = virgil.Cards.Create("AliceCard", aliceKey);
+            Assert.ThrowsAsync<Exceptions.AppCredentialsException>(() => virgil.Cards.RevokeAsync(aliceCard));
+        }
+    }
+}
