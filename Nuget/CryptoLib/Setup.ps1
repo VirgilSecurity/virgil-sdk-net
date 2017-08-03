20bc3c223c9c3590936700818b7480ecffc900ae
unlim-it <kurilenkodenis@gmail.com>
2017-08-03 12:25:33 +0300

    [SDK-102] Added support for Xamarin.iOS. Xamarin.Mac

--- Nuget/CryptoLib/Setup.ps1
+++ Nuget/CryptoLib/Setup.ps1
@@ -1,158 +1,127 @@
 Param (    
     [Parameter(Mandatory=$true)]
-	[string]$NuGetApiToken,
+    [string]$InputDir,
     [Parameter(Mandatory=$true)]
-    [string]$ActualCryptoLibVersion
+    [string]$OutputDir
 )
 
+Add-Type -assembly "system.io.compression"
 Add-Type -assembly "system.io.compression.filesystem"
+Add-Type -Path "./ICSharpCode.SharpZipLib.dll" 
 
-# Initialization
-# -------------------------------------------------------------------------------------------------------------
+# Initializing Parameters
+# -------------------------------------------------------------------------------------
 
-# Create temporary directories
+$CURRENT_DIR              = Get-Location
+$CRYPTO_LIB_DIR           = [System.IO.DirectoryInfo]::new($InputDir) 
+$CRYPTO_LIB_VERSION_FILE  = [System.IO.FileInfo]::new("$CRYPTO_LIB_DIR\VERSION")
+$CRYPTO_LIBS              = [ordered]@{ Platform = "net-windows";  Name = "net45";       TargetsFile = "$CURRENT_DIR\Targets\net45.targets" }, 
+                            [ordered]@{ Platform = "mono-android"; Name = "monoandroid"; TargetsFile = "$CURRENT_DIR\Targets\monoandroid.targets" }, 
+                            [ordered]@{ Platform = "mono-ios";     Name = "xamarinios";  TargetsFile = "$CURRENT_DIR\Targets\xamarinios.targets" }, 
+                            [ordered]@{ Platform = "mono-darwin";  Name = "xamarinmac";  TargetsFile = "$CURRENT_DIR\Targets\xamarinmac.targets" }
+$NUGET_PACKAGE_DIR        = New-Item -ItemType Directory -Force -Path $OutputDir 
 
-$CurrentDir = Get-Location
-$PackageDir = New-Item -ItemType Directory -Force -Path "$CurrentDir\package"
 
-
-$CryptoLibVersion = ""
-
-Try
-{
-    $CryptoLibVersion = [IO.File]::ReadAllText("$CurrentDir\VERSION").Trim()
-
+function Extract {
+    param([System.IO.FileInfo] $archive, [string] $destDir)
+        
+    # extract ZIP archive using native tools
+    if ($archive.Extension.ToLower() -eq ".zip") {
+        [io.compression.zipfile]::ExtractToDirectory($archive.FullName, $destDir)
+        return
+    }
     
-
-    if ([string]::IsNullOrWhiteSpace($CryptoLibVersion)){
-        throw [System.IO.FileNotFoundException] "Version could not be parsed"
+    # extract TGZ archive using CLI tool 
+    if ($archive.Extension.ToLower() -eq ".tgz") {
+        
+        $inStream = [System.IO.File]::OpenRead($archive.FullName)
+        $gzipStream = [ICSharpCode.SharpZipLib.GZip.GZipInputStream]::new($inStream)
+        $tarArchive = [ICSharpCode.SharpZipLib.Tar.TarArchive]::CreateInputTarArchive($gzipStream)
+        $tarArchive.ExtractContents($destDir)
+        $tarArchive.Close()
+        $gzipStream.Close()
+        $inStream.Close()
+        
+        return
     }
 }
-Catch
-{
-    throw [System.IO.FileNotFoundException] "Version is not provided"
+                   
+# Validating Input Parameters
+# -------------------------------------------------------------------------------------
+
+Write-Host $CRYPTO_LIB_DIR
+
+if (!$CRYPTO_LIB_DIR -or !$CRYPTO_LIB_DIR.Exists) {
+    Write-Error "Directory with Crypto libraries is not found"
+    exit 
 }
 
-
-# Extracting Crypto Librares
-# -------------------------------------------------------------------------------------------------------------
-
-
-function Expand-ZIPFile($file, $destination){
-    Try
-    {
-        [io.compression.zipfile]::ExtractToDirectory($file, $destination)
-    }
-    Catch
-    {
-        throw $_.Exception
-    }
+if (!$NUGET_PACKAGE_DIR -or !$NUGET_PACKAGE_DIR.Exists) {
+    Write-Error "Directory for Nuget package is not created"
+    exit 
 }
 
-function SmartCopy {
-    param([string] $sourceFile, [string] $destinationFile)
+if (!$CRYPTO_LIB_VERSION_FILE -or !$CRYPTO_LIB_VERSION_FILE.Exists) { 
+    Write-Error "File with Crypto version is not found"
+    exit 
+}
+
+## Extracting Crypto Libraries
+# -------------------------------------------------------------------------------------
+
+# load crypto lib version from file
+$version = [IO.File]::ReadAllText($CRYPTO_LIB_VERSION_FILE.FullName).Trim()    
+if ([string]::IsNullOrWhiteSpace($version)) {
+    Write-Error "File with CryptoLib version is empty or has invalid format"
+    exit
+}
+
+$cryptoNetDir = "$CRYPTO_LIB_DIR\net"
+$nugetLibDir = New-Item -ItemType Directory -Force -Path "$NUGET_PACKAGE_DIR\lib" 
+$nugetBuildDir = New-Item -ItemType Directory -Force -Path "$NUGET_PACKAGE_DIR\build" 
+$tempDir = New-Item -ItemType Directory -Force -Path "$CURRENT_DIR\_temp" 
+
+foreach ($lib in $CRYPTO_LIBS) {
+
+    $pattern = [string]::Format("virgil-crypto-{0}-{1}-[a-zA-Z0-9-_.]+.(zip|tgz)$", $version, $lib.Platform)
+
+    # find an archive file by specified name pattern
+    $file = Get-ChildItem -File $cryptoNetDir | Where-Object { $_.Name -match $pattern }
     
-    Try
-    {
-        New-Item -ItemType File -Path $destinationFile -Force | Out-Null -ErrorAction Stop
-        Copy-Item -Path $sourceFile -Destination $destinationFile -Force -ErrorAction Stop  
+    if (!$file){
+        Write-Error "File with pattern ($pattern) is not found in specified directory ($cryptoNetDir)"
+        exit
     }
-    Catch
-    {
-        throw $_.Exception
-    }  
+    
+    # remove all files in temp folder from previous iteration
+    Remove-Item "$tempDir\*" -Recurse
+
+    # extract archive to temp directory
+    Extract $file $tempDir 
+
+    # search in temp directory for Virgil.Crypto.dll assembly
+    $assembly = Get-ChildItem -Path $tempDir -Filter Virgil.Crypto.dll -Recurse -Force   
+     
+    if (!$assembly){
+        Write-Error "Assembly dll is not found in unzipped archive"
+        exit
+    } 
+
+    # move the assembly to specified output folder '%OUTPUT_DIR%\lib\%PLATFORM%\'
+
+    $nugetAssemblyFile = [string]::Format("{0}\{1}\{2}", $nugetLibDir, $lib.Name, $assembly.Name)
+    
+    New-Item -ItemType File -Path $nugetAssemblyFile -Force 
+    Move-Item $assembly.FullName $nugetAssemblyFile -Force      
+    
+    # move the targets and native libs to specified output folder '%OUTPUT_DIR%\build\%PLATFORM%\'
+    
+    $nugetTargetsFile = [System.IO.FileInfo]::new([string]::Format("{0}\{1}\Virgil.Crypto.targets", $nugetBuildDir, $lib.Name))
+    
+    New-Item -ItemType File -Path $nugetTargetsFile -Force 
+    Copy-Item $lib.TargetsFile $nugetTargetsFile -Force  
+    Get-ChildItem -Path "$($assembly.Directory.FullName)\" -Recurse | Move-Item -Destination $nugetTargetsFile.Directory.FullName        
 }
 
-$NetWindows_Package_Path = Get-ChildItem $CurrentDir\net | Where-Object {$_.Name -match "virgil-crypto-$CryptoLibVersion-net-windows-[0-9]+.[0-9]+.zip$"}
-$MonoAndroid_Package_Path = Get-ChildItem $CurrentDir\net | Where-Object {$_.Name -match "virgil-crypto-$CryptoLibVersion-mono-android-[0-9]+.tgz$"}
-$MonoiOS_Package_Path = Get-ChildItem $CurrentDir\net | Where-Object {$_.Name -match "virgil-crypto-$CryptoLibVersion-mono-ios-[0-9]+.[0-9]+.tgz$"}
-$MonoMac_Package_Path = Get-ChildItem $CurrentDir\net | Where-Object {$_.Name -match "virgil-crypto-$CryptoLibVersion-mono-darwin-[0-9]+.[0-9]+-x86_64.tgz$"}
-
-$XamarinMac_Package_Path = $MonoMac_Package_Path
-
-if ([string]::IsNullOrWhiteSpace($NetWindows_Package_Path)){
-    throw [System.IO.FileNotFoundException] "Windows .Net package is not found"
-}
-
-if ([string]::IsNullOrWhiteSpace($MonoAndroid_Package_Path)){
-    throw [System.IO.FileNotFoundException] "Android Mono package is not found"
-}
-
-if ([string]::IsNullOrWhiteSpace($MonoiOS_Package_Path)){
-    throw [System.IO.FileNotFoundException] "iOS Mono package is not found"
-}
-
-if ([string]::IsNullOrWhiteSpace($MonoMac_Package_Path)){
-    throw [System.IO.FileNotFoundException] "Xamarin Mac package is not found"
-}
-
-if ([string]::IsNullOrWhiteSpace($XamarinMac_Package_Path)){
-    throw [System.IO.FileNotFoundException] "Xamarin Mac package is not found"
-}
-
-Expand-ZIPFile -File "$CurrentDir\net\$NetWindows_Package_Path" -Destination $CurrentDir
-
-$Result = Invoke-Command { .\TarTool.exe "$CurrentDir\net\$MonoAndroid_Package_Path" } -ErrorAction Stop
-if ($Result){
-    throw [System.Exception] "Extracting $MonoAndroid_Package_Path file is failed"
-}
-
-$Result = Invoke-Command { .\TarTool.exe "$CurrentDir\net\$MonoiOS_Package_Path" } -ErrorAction Stop
-if ($Result){
-    throw [System.Exception] "Extracting $MonoiOS_Package_Path file is failed"
-}
-
-$Result = Invoke-Command { .\TarTool.exe "$CurrentDir\net\$MonoMac_Package_Path" } -ErrorAction Stop
-if ($Result){
-    throw [System.Exception] "Extracting $MonoMac_Package_Path file is failed"
-}
-
-$NetWindows_Package_Name  = [System.IO.Path]::GetFileNameWithoutExtension($NetWindows_Package_Path)
-$MonoAndroid_Package_Name = [System.IO.Path]::GetFileNameWithoutExtension($MonoAndroid_Package_Path)
-$MonoiOS_Package_Name     = [System.IO.Path]::GetFileNameWithoutExtension($MonoiOS_Package_Path)
-$MonoMac_Package_Name     = [System.IO.Path]::GetFileNameWithoutExtension($MonoMac_Package_Path)
-
-# Copy Mono packages
-
-SmartCopy "$CurrentDir\$MonoAndroid_Package_Name\lib\Virgil.Crypto.dll" "$PackageDir\lib\MonoAndroid\Virgil.Crypto.dll"
-SmartCopy "$CurrentDir\$MonoAndroid_Package_Name\lib\virgil_crypto_java_jni.jar" "$PackageDir\build\native\android\virgil_crypto_java_jni.jar"
-SmartCopy "$CurrentDir\MonoAndroid.targets" "$PackageDir\build\MonoAndroid\Virgil.Crypto.targets"
-
-SmartCopy "$CurrentDir\$MonoiOS_Package_Name\lib\Virgil.Crypto.dll" "$PackageDir\lib\MonoTouch\Virgil.Crypto.dll"
-SmartCopy "$CurrentDir\$MonoiOS_Package_Name\lib\libvirgil_crypto_net.so" "$PackageDir\build\native\ios\libvirgil_crypto_net.so"
-SmartCopy "$CurrentDir\MonoTouch.targets" "$PackageDir\build\MonoTouch\Virgil.Crypto.targets"
-
-SmartCopy "$CurrentDir\$MonoMac_Package_Name\lib\Virgil.Crypto.dll" "$PackageDir\lib\MonoMac\Virgil.Crypto.dll"
-SmartCopy "$CurrentDir\$MonoMac_Package_Name\lib\libvirgil_crypto_net.so" "$PackageDir\build\native\mac\libvirgil_crypto_net.so"
-SmartCopy "$CurrentDir\MonoMac.targets" "$PackageDir\build\MonoMac\Virgil.Crypto.targets"
-
-# Copy Xamarin packages
-
-SmartCopy "$CurrentDir\$MonoiOS_Package_Name\lib\Virgil.Crypto.dll" "$PackageDir\lib\xamarinios\Virgil.Crypto.dll"
-SmartCopy "$CurrentDir\MonoTouch.targets" "$PackageDir\build\xamarinios\Virgil.Crypto.targets"
-
-SmartCopy "$CurrentDir\$MonoMac_Package_Name\lib\Virgil.Crypto.dll" "$PackageDir\lib\xamarinmac\Virgil.Crypto.dll"
-SmartCopy "$CurrentDir\MonoMac.targets" "$PackageDir\build\xamarinmac\Virgil.Crypto.targets"
-
-# Copy NetFX packages
-
-SmartCopy "$CurrentDir\$NetWindows_Package_Name\lib\Virgil.Crypto.dll" "$PackageDir\lib\portable-net4+sl4+wp7+win8+wpa81\Virgil.Crypto.dll"
-SmartCopy "$CurrentDir\$NetWindows_Package_Name\lib\x64\virgil_crypto_net.dll" "$PackageDir\build\native\win\x64\virgil_crypto_net.dll"
-SmartCopy "$CurrentDir\$NetWindows_Package_Name\lib\x86\virgil_crypto_net.dll" "$PackageDir\build\native\win\x86\virgil_crypto_net.dll"
-SmartCopy "$CurrentDir\PortableNet.targets" "$PackageDir\build\portable-net4+sl4+wp7+win8+wpa81\Virgil.Crypto.targets"
-
-# Prepare NuGet package
-# ---------------------------------------------------------------------------------------------------------------------------------
-
-(Get-Content "$CurrentDir\Package.nuspec").replace("%version%", $ActualCryptoLibVersion) | Set-Content "$PackageDir\Package.nuspec"
-
-# Updating NuGet package
-# ---------------------------------------------------------------------------------------------------------------------------------
-
-Invoke-Command {.\NuGet.exe update -Self} -ErrorAction Stop
-
-# Publish NuGet package
-# -------------------------------------------------------------------------------------------------------------
-
-Invoke-Command {.\NuGet.exe pack "$PackageDir\Package.nuspec" -Verbosity Detailed} -ErrorAction Stop
-Invoke-Command {.\NuGet.exe push ".\Virgil.Crypto.$ActualCryptoLibVersion.nupkg" -Source "nuget.org" -ApiKey $NuGetApiToken } -ErrorAction Stop
+# replace version in nuspec file and copy it to the output folder '%OUTPUT_DIR%\'
+(Get-Content "$CURRENT_DIR\Package.nuspec").replace("%version%", $version) | Set-Content "$NUGET_PACKAGE_DIR\Package.nuspec"
