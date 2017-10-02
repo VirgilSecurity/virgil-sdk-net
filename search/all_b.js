e8922e8eee1309d5b1dcb8eb3b242f3c568f18fe
AppVeyor <marfachaiko@gmail.com>
2017-10-02 13:43:43 +0000

    Deployment Bot (from AppVeyor CI)

--- search/all_b.js
+++ search/all_b.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['reason',['Reason',['../class_virgil_1_1_s_d_k_1_1_client_1_1_revoke_card_snapshot_model.html#aa70089263549f1a8bf542df3c5800fb7',1,'Virgil::SDK::Client::RevokeCardSnapshotModel']]],
+  ['receiverid',['ReceiverId',['../class_virgil_1_1_s_d_k_1_1_cryptography_1_1_private_key.html#a17e16dc3ca34925e865454ac49c97a72',1,'Virgil::SDK::Cryptography::PrivateKey']]],
   ['recipientsnotfoundexception',['RecipientsNotFoundException',['../class_virgil_1_1_s_d_k_1_1_exceptions_1_1_recipients_not_found_exception.html#ab25444100b7dce65b39f334a191776d8',1,'Virgil::SDK::Exceptions::RecipientsNotFoundException']]],
   ['recipientsnotfoundexception',['RecipientsNotFoundException',['../class_virgil_1_1_s_d_k_1_1_exceptions_1_1_recipients_not_found_exception.html',1,'Virgil::SDK::Exceptions']]],
   ['relationexception',['RelationException',['../class_virgil_1_1_s_d_k_1_1_exceptions_1_1_relation_exception.html',1,'Virgil::SDK::Exceptions']]],
@@ -14,6 +15,6 @@ var searchData=
   ['revokecardsnapshotmodel',['RevokeCardSnapshotModel',['../class_virgil_1_1_s_d_k_1_1_client_1_1_revoke_card_snapshot_model.html',1,'Virgil::SDK::Client']]],
   ['revokeglobalasync',['RevokeGlobalAsync',['../interface_virgil_1_1_s_d_k_1_1_i_cards_manager.html#ac2f959a90a3cc09d047ebf102f390e1c',1,'Virgil::SDK::ICardsManager']]],
   ['revokeglobalcardasync',['RevokeGlobalCardAsync',['../class_virgil_1_1_s_d_k_1_1_client_1_1_virgil_client.html#a5af6af7d586dc6ad4771f8ee4bd9f63c',1,'Virgil::SDK::Client::VirgilClient']]],
-  ['revokeglobalcardrequest',['RevokeGlobalCardRequest',['../class_virgil_1_1_s_d_k_1_1_client_1_1_revoke_global_card_request.html',1,'Virgil::SDK::Client']]],
-  ['revokeglobalcardrequest',['RevokeGlobalCardRequest',['../class_virgil_1_1_s_d_k_1_1_client_1_1_revoke_global_card_request.html#ae04b823494a9cd0080256e23ae33130b',1,'Virgil.SDK.Client.RevokeGlobalCardRequest.RevokeGlobalCardRequest(string stringifiedRequest)'],['../class_virgil_1_1_s_d_k_1_1_client_1_1_revoke_global_card_request.html#a126e15080bf558200c8e4731323e0d20',1,'Virgil.SDK.Client.RevokeGlobalCardRequest.RevokeGlobalCardRequest(string cardId, RevocationReason reason, string validationToken)']]]
+  ['revokeglobalcardrequest',['RevokeGlobalCardRequest',['../class_virgil_1_1_s_d_k_1_1_client_1_1_revoke_global_card_request.html#ae04b823494a9cd0080256e23ae33130b',1,'Virgil.SDK.Client.RevokeGlobalCardRequest.RevokeGlobalCardRequest(string stringifiedRequest)'],['../class_virgil_1_1_s_d_k_1_1_client_1_1_revoke_global_card_request.html#a126e15080bf558200c8e4731323e0d20',1,'Virgil.SDK.Client.RevokeGlobalCardRequest.RevokeGlobalCardRequest(string cardId, RevocationReason reason, string validationToken)']]],
+  ['revokeglobalcardrequest',['RevokeGlobalCardRequest',['../class_virgil_1_1_s_d_k_1_1_client_1_1_revoke_global_card_request.html',1,'Virgil::SDK::Client']]]
 ];
--- search/classes_5.js
+++ search/classes_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['privatekey',['PrivateKey',['../class_virgil_1_1_s_d_k_1_1_cryptography_1_1_private_key.html',1,'Virgil::SDK::Cryptography']]],
   ['publishcardrequest',['PublishCardRequest',['../class_virgil_1_1_s_d_k_1_1_client_1_1_publish_card_request.html',1,'Virgil::SDK::Client']]],
   ['publishcardsnapshotmodel',['PublishCardSnapshotModel',['../class_virgil_1_1_s_d_k_1_1_client_1_1_publish_card_snapshot_model.html',1,'Virgil::SDK::Client']]],
   ['publishglobalcardrequest',['PublishGlobalCardRequest',['../class_virgil_1_1_s_d_k_1_1_client_1_1_publish_global_card_request.html',1,'Virgil::SDK::Client']]]
