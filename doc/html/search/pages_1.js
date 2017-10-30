cb23c48ad4177fd55034fe316b6f7937612b7797
AppVeyor <marfachaiko@gmail.com>
2017-10-30 12:15:57 +0000

    Deployment Bot (from AppVeyor CI)

---
+++ doc/html/search/pages_1.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['client_20configuration',['Client Configuration',['../md_docs_guides_configuration_client.html',1,'']]],
+  ['client_20configuration',['Client Configuration',['../md_docs_guides_configuration_client-pfs.html',1,'']]],
+  ['creating_20signature',['Creating Signature',['../md_docs_guides_signature_creating-signature.html',1,'']]],
+  ['creating_20card',['Creating Card',['../md_docs_guides_virgil-card_creating-card.html',1,'']]],
+  ['creating_20global_20card',['Creating Global Card',['../md_docs_guides_virgil-card_creating-global-card.html',1,'']]],
+  ['copyright_202017_20virgil_20security',['Copyright 2017 Virgil Security',['../md_LICENSE.html',1,'']]]
+];
--- doc/html/search/pages_9.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['revoking_20card',['Revoking Card',['../md_documentation_guides_virgil-card_revoking-card.html',1,'']]],
-  ['revoking_20global_20card',['Revoking Global Card',['../md_documentation_guides_virgil-card_revoking-global-card.html',1,'']]]
-];
