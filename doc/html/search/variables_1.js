fa6c5164a96bdc4d6c4d092062138d6bc82655e5
AppVeyor <marfachaiko@gmail.com>
2017-10-31 13:34:19 +0000

    Deployment Bot (from AppVeyor CI)

--- doc/html/search/variables_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['id',['Id',['../class_virgil_1_1_s_d_k_1_1_virgil_card.html#ab7484c51482e519b1cd5241e8d246d73',1,'Virgil::SDK::VirgilCard']]],
-  ['identity',['Identity',['../class_virgil_1_1_s_d_k_1_1_virgil_card.html#a1959cf354bf98675058ebadb6330b7bf',1,'Virgil::SDK::VirgilCard']]],
-  ['identitytype',['IdentityType',['../class_virgil_1_1_s_d_k_1_1_virgil_card.html#adf3ebbe2b79ae82650ac4c3e15cd56d5',1,'Virgil::SDK::VirgilCard']]]
-];
