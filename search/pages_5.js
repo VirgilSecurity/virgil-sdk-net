634040d1df1cb0318d1d9490f17a7f0e4b64df50
AppVeyor <marfachaiko@gmail.com>
2017-10-31 13:06:56 +0000

    Deployment Bot (from AppVeyor CI)

---
+++ search/pages_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['generating_20keys',['Generating Keys',['../md_docs_guides_virgil-key_generating-key.html',1,'']]]
+];
---
+++ search/properties_3.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['identities',['Identities',['../class_virgil_1_1_s_d_k_1_1_client_1_1_search_criteria.html#a09c882429c038443f847daf06fff3dce',1,'Virgil::SDK::Client::SearchCriteria']]],
+  ['identity',['Identity',['../class_virgil_1_1_s_d_k_1_1_identity_verification_attempt.html#a76cf37675041e6a1cd91eb18cea117c6',1,'Virgil::SDK::IdentityVerificationAttempt']]],
+  ['identitytype',['IdentityType',['../class_virgil_1_1_s_d_k_1_1_client_1_1_search_criteria.html#a995ca10aca7906a5013174ab02204a55',1,'Virgil.SDK.Client.SearchCriteria.IdentityType()'],['../class_virgil_1_1_s_d_k_1_1_identity_verification_attempt.html#aa6bbfc005c1e6114347b2affd168234e',1,'Virgil.SDK.IdentityVerificationAttempt.IdentityType()']]],
+  ['invalidcards',['InvalidCards',['../class_virgil_1_1_s_d_k_1_1_exceptions_1_1_card_validation_exception.html#a899d1717bdd2433a6309d2fce4e41e53',1,'Virgil::SDK::Exceptions::CardValidationException']]]
+];
