abfa7581c752d32675707f73fb7440f731c9ef9a
Vasilina Bezuglaya <Vasilina Bezuglaya>
2018-02-22 20:57:49 +0500

    nuget config

--- Nuget/virgil-sdk.py
+++ Nuget/virgil-sdk.py
@@ -2,6 +2,7 @@ import msbuilder
 
 proj = r'..\SDK\Source\Virgil.SDK.NetFx\Virgil.SDK.NetFx.csproj'
 spec = r'..\SDK\Source\Virgil.SDK.NetFx\Virgil.SDK.NetFx.nuspec'
+nuget = r'C:\Users\Vasilina\Documents\projects\sdk-net2\virgil-sdk-net\SDK\Source\.nuget\NuGet.exe'
 output = r'.\output'
 
 builder = msbuilder.MsBuilder()
