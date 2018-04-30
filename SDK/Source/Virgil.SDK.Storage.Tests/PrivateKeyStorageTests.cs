89142d520ea2bbe2c0e205f65b8bb9335e2eaada
Vasilina <marfachaiko@gmail.com>
2018-04-30 18:29:24 +0500

    [BUGFIX] wrong JWT format

---
+++ SDK/Source/Virgil.SDK.Storage.Tests/PrivateKeyStorageTests.cs
@@ -0,0 +1,39 @@
+﻿using Bogus;
+using NUnit.Framework;
+using System;
+using Virgil.Crypto;
+using Virgil.SDK.Storage;
+
+namespace Virgil.SDK.Tests.Shared
+{
+    [NUnit.Framework.TestFixture]
+    public class PrivateKeyStorageTestsNet
+    {
+        private readonly Faker faker = new Faker();
+
+        [Test]
+        [Ignore("can work only on local machine which has non-empty old keystorage")]
+        public void Load_Should_Return_EqualOldPrivateKey_If_StorageHasSHA256Exporter()
+        {
+            var exporterPass = faker.Random.AlphaNumeric(10);
+            var storagePass = faker.Random.AlphaNumeric(10);
+            var crypto = new VirgilCrypto();
+            var cryptoSHA256 = new VirgilCrypto() { UseSHA256Fingerprints = true };
+
+            var exporter = new VirgilPrivateKeyExporter(cryptoSHA256, exporterPass);
+            var privateKeyStorage = new PrivateKeyStorage(exporter, storagePass);
+
+            var storageReader =
+                new KeyStorageReaderV4(AppSettings.OldKeyStoragePath, true);
+            var (oldKeyData, oldKeydata) = storageReader.Load(AppSettings.OldKeyAliase);
+            var key = cryptoSHA256.ImportPrivateKey(oldKeyData);
+            var oldKeyAliase = faker.Random.AlphaNumeric(10);
+            privateKeyStorage.Store(key, oldKeyAliase);
+            var loadedOldKey = privateKeyStorage.Load(oldKeyAliase).Item1;
+            
+            //Assert.IsTrue(((PrivateKey)key).Id.SequenceEqual(((PrivateKey)loadedOldKey).Id));
+            //Assert.IsTrue(((PrivateKey)key).RawKey.SequenceEqual(((PrivateKey)loadedOldKey).RawKey));
+            privateKeyStorage.Delete(oldKeyAliase);
+        }
+    }
+}
