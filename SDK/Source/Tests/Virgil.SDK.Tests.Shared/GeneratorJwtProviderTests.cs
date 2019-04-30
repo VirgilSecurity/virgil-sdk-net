27f8f0144fa0c10f3c9c6e4adf3ce92135911734
Vasilina <marfachaiko@gmail.com>
2019-04-30 17:05:33 +0500

    GeneratorJwtProvider

---
+++ SDK/Source/Tests/Virgil.SDK.Tests.Shared/GeneratorJwtProviderTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Virgil.Crypto;
+using Virgil.SDK.Common;
+using Virgil.SDK.Web.Authorization;
+
+namespace Virgil.SDK.Tests.Shared
+{
+    [TestFixture]
+    public class GeneratorJwtProviderTests
+    {
+        public GeneratorJwtProviderTests()
+        {
+        }
+
+        [Test]
+        public async Task generatorJwtProvider_Should_GenerateValidJwt()
+        {
+            var defaultIdentity = "my_default_identity";
+            var identity = "identity";
+            var signer = new VirgilAccessTokenSigner();
+            var builder = new JwtGenerator(
+                AppSettings.AppId,
+                IntegrationHelper.ApiPrivateKey(),
+                AppSettings.ApiPublicKeyId,
+                TimeSpan.FromMinutes(5),
+                signer);
+            var generatorJwtProvider = new GeneratorJwtProvider(builder, defaultIdentity);
+            var jwt = (Jwt)(await generatorJwtProvider.GetTokenAsync(new TokenContext(identity, "get")));
+            Assert.Equals(identity, jwt.Identity());
+            Assert.Equals(AppSettings.AppId, jwt.BodyContent.AppId);
+            Assert.Null(jwt.BodyContent.AdditionalData);
+
+            var crypto = new VirgilCrypto();
+            var jwtVerifier = new JwtVerifier(
+                signer,
+                crypto.ImportPublicKey(
+                    Bytes.FromString(AppSettings.ImportedAccessPublicKey, StringEncoding.BASE64)),
+                AppSettings.ImportedAccessPublicKeyId);
+            Assert.IsTrue(jwtVerifier.VerifyToken(jwt));
+        }
+
+        [Test]
+        public async Task generatorJwtProvider_Should_GenerateValidJwtWithAdditionalData()
+        {
+            var defaultIdentity = "my_default_identity";
+            var identity = "identity";
+            var additionalData = new Dictionary<object, object>() { {"field_1", "data"}};
+            var signer = new VirgilAccessTokenSigner();
+            var builder = new JwtGenerator(
+                AppSettings.AppId,
+                IntegrationHelper.ApiPrivateKey(),
+                AppSettings.ApiPublicKeyId,
+                TimeSpan.FromMinutes(5),
+                signer);
+            var generatorJwtProvider = new GeneratorJwtProvider(builder, defaultIdentity, additionalData);
+            var jwt = (Jwt)(await generatorJwtProvider.GetTokenAsync(new TokenContext(identity, "get")));
+            Assert.Equals(identity, jwt.Identity());
+            Assert.Equals(AppSettings.AppId, jwt.BodyContent.AppId);
+            Assert.Equals(additionalData, jwt.BodyContent.AdditionalData);
+            var crypto = new VirgilCrypto();
+            var jwtVerifier = new JwtVerifier(
+                signer,
+                crypto.ImportPublicKey(
+                    Bytes.FromString(AppSettings.ImportedAccessPublicKey, StringEncoding.BASE64)),
+                AppSettings.ImportedAccessPublicKeyId);
+            Assert.IsTrue(jwtVerifier.VerifyToken(jwt));
+        }
+
+        [Test]
+        public async Task generatorJwtProvider_Should_GenerateJwtWithDefaultIdentity()
+        {
+            var defaultIdentity = "my_default_identity";
+            var additionalData = new Dictionary<object, object>() { { "field_1", "data" } };
+            var signer = new VirgilAccessTokenSigner();
+            var builder = new JwtGenerator(
+                AppSettings.AppId,
+                IntegrationHelper.ApiPrivateKey(),
+                AppSettings.ApiPublicKeyId,
+                TimeSpan.FromMinutes(5),
+                signer);
+            var generatorJwtProvider = new GeneratorJwtProvider(builder, defaultIdentity, additionalData);
+            var jwt = (Jwt)(await generatorJwtProvider.GetTokenAsync(new TokenContext(null, "get")));
+            Assert.Equals(defaultIdentity, jwt.Identity());
+            Assert.Equals(AppSettings.AppId, jwt.BodyContent.AppId);
+            Assert.Equals(additionalData, jwt.BodyContent.AdditionalData);
+            var crypto = new VirgilCrypto();
+            var jwtVerifier = new JwtVerifier(
+                signer,
+                crypto.ImportPublicKey(
+                    Bytes.FromString(AppSettings.ImportedAccessPublicKey, StringEncoding.BASE64)),
+                AppSettings.ImportedAccessPublicKeyId);
+            Assert.IsTrue(jwtVerifier.VerifyToken(jwt));
+        }
+    }
+}
---
+++ SDK/Source/Virgil.SDK.Shared/Web/Authorization/GeneratorJwtProvider.cs
@@ -0,0 +1,91 @@
+﻿#region Copyright (C) Virgil Security Inc.
+// Copyright (C) 2015-2019 Virgil Security Inc.
+// 
+// Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions 
+// are met:
+// 
+//   (1) Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//   
+//   (2) Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in
+//   the documentation and/or other materials provided with the
+//   distribution.
+//   
+//   (3) Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived 
+//   from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#endregion
+
+namespace Virgil.SDK
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Virgil.SDK.Web.Authorization;
+
+    /// <summary>
+    /// The <see cref="GeneratorJwtProvider"/> class provides an opportunity to  
+    /// generate <see cref="IAccessToken" /> using provided <see cref="Web.Authorization.JwtGenerator" />. 
+    /// </summary>
+    public class GeneratorJwtProvider : IAccessTokenProvider
+    {
+        public string DefaultIdentity { get; private set; }
+        public Dictionary<object, object> AdditionalData;
+        public JwtGenerator JwtGenerator { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeneratorJwtProvider" /> class.
+        /// </summary>
+        /// <param name="jwtGenerator">will generate new JWT.</param>
+        /// <param name="defaultIdentity">identity which will be used in token
+        ///  generation by default.</param>
+        /// <param name="additionalData">dictionary with additional data which will
+        ///  be used in token generation.</param>
+        public GeneratorJwtProvider(
+            JwtGenerator jwtGenerator, 
+            string defaultIdentity, 
+            Dictionary<object, object> additionalData = null)
+        {
+            if (string.IsNullOrWhiteSpace(defaultIdentity))
+            {
+                throw new ArgumentException(nameof(defaultIdentity));
+            }
+            this.DefaultIdentity = defaultIdentity;
+            this.JwtGenerator = jwtGenerator ?? throw new ArgumentNullException(nameof(jwtGenerator));
+            this.AdditionalData = additionalData;
+        }
+
+        /// <summary>
+        /// Generates new JWT using specified identity from content or default identity and additional data.
+        /// </summary>
+        /// <param name="context">includes an identity to generate with.</param>
+        /// <returns>a new instanse of <see cref="IAccessToken"/>.</returns>
+        public Task<IAccessToken> GetTokenAsync(TokenContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            var token = this.JwtGenerator.GenerateToken(context.Identity ?? this.DefaultIdentity, AdditionalData);
+            return Task.FromResult((IAccessToken)token);
+        }
+    }
+}
